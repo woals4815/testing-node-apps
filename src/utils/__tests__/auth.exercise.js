0384f35a5295ae0bc448f891481212de5bc53256
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-10 21:44:53 +0900

    add tests in auth.exercise.js

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,21 +1,38 @@
 // Testing Pure Functions
 
 // 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
 
 // 🐨 import the function that we're testing
+import {isPasswordAllowed} from '../auth';
 // 💰 import {isPasswordAllowed} from '../auth'
 
 // 🐨 write tests for valid and invalid passwords
+
+describe('isAllowed test', () => {
+  const validPWs = ['!aBc123'];
+  const invalidPWs = [
+    'a2c!',
+    '123456!',
+    'ABCdef!',
+    'abc123!',
+    'ABC123!',
+    'ABCdef123',
+  ];
+  validPWs.forEach((pw) => {
+    test(`valid pw test: ${pw}`, () => {
+      expect(isPasswordAllowed(pw)).toBe(true);
+    });
+  });
+  invalidPWs.forEach((pw) => {
+    test(`invalid pw test ${pw}`, () => {
+      expect(isPasswordAllowed(pw)).toBe(false);
+    });
+  });
+});
+
 // 💰 here are some you can use:
 //
 // valid:
 // - !aBc123
 //
 // invalid:
-// - a2c! // too short
-// - 123456! // no alphabet characters
-// - ABCdef! // no numbers
-// - abc123! // no uppercase letters
-// - ABC123! // no lowercase letters
-// - ABCdef123 // no non-alphanumeric characters
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,41 +1,41 @@
-import crypto from 'crypto'
-import expressJWT from 'express-jwt'
-import LocalStrategy from 'passport-local'
-import {omit} from 'lodash'
-import jwt from 'jsonwebtoken'
-import * as usersDB from '../db/users'
+import crypto from 'crypto';
+import expressJWT from 'express-jwt';
+import LocalStrategy from 'passport-local';
+import {omit} from 'lodash';
+import jwt from 'jsonwebtoken';
+import * as usersDB from '../db/users';
 
 // in a real app this would be set in an environment variable
-const secret = 'secret'
+const secret = 'secret';
 
 // reducing the iterations to 1 in non-production environments to make it faster
-const iterations = process.env.NODE_ENV === 'production' ? 1000 : 1
+const iterations = process.env.NODE_ENV === 'production' ? 1000 : 1;
 
 // seconds/minute * minutes/hour * hours/day * 60 days
-const sixtyDaysInSeconds = 60 * 60 * 24 * 60
+const sixtyDaysInSeconds = 60 * 60 * 24 * 60;
 // to keep our tests reliable, we'll use the requireTime if we're not in production
 // and we'll use Date.now() if we are.
-const requireTime = Date.now()
+const requireTime = Date.now();
 const now = () =>
-  process.env.NODE_ENV === 'production' ? Date.now() : requireTime
+  process.env.NODE_ENV === 'production' ? Date.now() : requireTime;
 
 function getSaltAndHash(password) {
-  const salt = crypto.randomBytes(16).toString('hex')
+  const salt = crypto.randomBytes(16).toString('hex');
   const hash = crypto
     .pbkdf2Sync(password, salt, iterations, 512, 'sha512')
-    .toString('hex')
-  return {salt, hash}
+    .toString('hex');
+  return {salt, hash};
 }
 
 function isPasswordValid(password, {salt, hash}) {
   return (
     hash ===
     crypto.pbkdf2Sync(password, salt, iterations, 512, 'sha512').toString('hex')
-  )
+  );
 }
 
 function getUserToken({id, username}) {
-  const issuedAt = Math.floor(now() / 1000)
+  const issuedAt = Math.floor(now() / 1000);
   return jwt.sign(
     {
       id,
@@ -44,30 +44,30 @@ function getUserToken({id, username}) {
       exp: issuedAt + sixtyDaysInSeconds,
     },
     secret,
-  )
+  );
 }
 
-const authMiddleware = expressJWT({algorithms: ['HS256'], secret})
+const authMiddleware = expressJWT({algorithms: ['HS256'], secret});
 
 function getLocalStrategy() {
   return new LocalStrategy(async (username, password, done) => {
-    let user
+    let user;
     try {
-      user = await usersDB.readByUsername(username)
+      user = await usersDB.readByUsername(username);
     } catch (error) {
-      return done(error)
+      return done(error);
     }
     if (!user || !isPasswordValid(password, user)) {
       return done(null, false, {
         message: 'username or password is invalid',
-      })
+      });
     }
-    return done(null, userToJSON(user))
-  })
+    return done(null, userToJSON(user));
+  });
 }
 
 function userToJSON(user) {
-  return omit(user, ['exp', 'iat', 'hash', 'salt'])
+  return omit(user, ['exp', 'iat', 'hash', 'salt']);
 }
 
 function isPasswordAllowed(password) {
@@ -81,7 +81,7 @@ function isPasswordAllowed(password) {
     /[A-Z]/.test(password) &&
     // lowercase letter
     /[a-z]/.test(password)
-  )
+  );
 }
 
 export {
@@ -91,4 +91,4 @@ export {
   getLocalStrategy,
   getUserToken,
   isPasswordAllowed,
-}
+};
